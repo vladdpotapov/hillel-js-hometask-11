7f352040edc4122fd2c83c4155d90ae962901163
PerSick <vlad461@ukr.net>
2020-02-18 20:05:27 +0300

    delete button

--- js/functions.js
+++ js/functions.js
@@ -22,9 +22,20 @@ function insertUserDataToRow() {
         cellActions.classList.add('table__cell');
         row.append(cellActions);
 
-        cellActions.append(createRowActionButton('View'));
-        cellActions.append(createRowActionButton('Edit'));
-        cellActions.append(createRowActionButton('Delete'));
+        let tableButtonView   = createRowActionButton('View');
+        tableButtonView.addEventListener('click', () => showUserInfoCard(i));
+
+        let tableButtonEdit   = createRowActionButton('Edit');
+        tableButtonEdit.addEventListener('click', () => showUserEditCard(i));
+
+        let tableButtonDelete = createRowActionButton('Delete');
+        tableButtonDelete.addEventListener('click', () => removeUserObject(i));
+
+        tableButtonAdd.addEventListener('click', () => showAddNewUserCard(i));
+
+        cellActions.append(tableButtonView);
+        cellActions.append(tableButtonEdit);
+        cellActions.append(tableButtonDelete);
     }
 }
 
@@ -35,3 +46,266 @@ function createRowActionButton(value) {
     button.classList.add('table__button');
     return button;
 }
+
+function showUserInfoCard(i) {
+    infoWindow.innerHTML = '';
+
+    let userInfoTable = document.createElement('table');
+    userInfoTable.classList.add('table');
+    infoWindow.append(userInfoTable);
+
+    let userInfoCell;
+    let userInfoRow;
+    for (let key in users[i]) {
+        userInfoRow = document.createElement('tr');
+        userInfoRow.classList.add('table__row');
+        userInfoTable.append(userInfoRow);
+    
+        userInfoCell = document.createElement('td'); 
+        userInfoCell.classList.add('table__cell');
+        userInfoCell.innerHTML = `${key}: ${users[i][key]}`;
+        userInfoRow.append(userInfoCell);
+    }
+
+    let userInfoCloseButton = document.createElement('input');
+    userInfoCloseButton.setAttribute('type', 'button');
+    userInfoCloseButton.setAttribute('value', 'Close');
+    userInfoCloseButton.classList.add('table__button');
+    userInfoCloseButton.addEventListener('click', () => closeWindow(userInfoTable));
+    userInfoTable.append(userInfoCloseButton);
+}
+
+function showUserEditCard(i) {
+    infoWindow.innerHTML = '';
+
+    let userInfoTable = document.createElement('table');
+    userInfoTable.classList.add('table');
+    infoWindow.append(userInfoTable);
+
+    let userInfoCell;
+    let userInfoRow;
+    let userEditInput;
+    for (let key in users[i]) {
+        userInfoRow = document.createElement('tr');
+        userInfoRow.classList.add('table__row');
+        userInfoTable.append(userInfoRow);
+    
+        userInfoCell = document.createElement('td'); 
+        userInfoCell.classList.add('table__cell');
+        userInfoCell.innerHTML = `${key}: `;
+        userInfoRow.append(userInfoCell);
+
+        userEditInput = document.createElement('input');
+        userEditInput.setAttribute('value', users[i][key]);
+        userEditInput.classList.add('edit__inputs');
+        userInfoTable.append(userEditInput);
+    }
+
+    let userInfoCloseButton = document.createElement('input');
+    userInfoCloseButton.setAttribute('type', 'button');
+    userInfoCloseButton.setAttribute('value', 'Close');
+    userInfoCloseButton.classList.add('table__button');
+    userInfoCloseButton.addEventListener('click', () => closeWindow(userInfoTable));
+    userInfoTable.append(userInfoCloseButton);
+
+    let userInfoSaveButton = document.createElement('input');
+    userInfoSaveButton.setAttribute('type', 'button');
+    userInfoSaveButton.setAttribute('value', 'Save');
+    userInfoSaveButton.classList.add('table__button');
+    userInfoSaveButton.addEventListener('click', function() {
+        let userInputs = document.querySelectorAll('.edit__inputs');
+        let array       = Object.values(users[i]);
+
+        for (let i = 0; i < userInputs.length; i++) {
+            if (array[i] !== userInputs[i].value) {
+                array[i] = userInputs[i].value;
+            } 
+            // console.log(array[i]);
+        }
+
+        // const obj = {id: null, name: null};
+        // const arr = ['11', 'qwerty'];
+
+        // Object.keys(obj).forEach(function(key, index){
+        // obj[key] = arr[index];
+        // });
+
+        const objProp = {id: null, name: null, age: null, job: null};
+        Object.keys(objProp).forEach(function(key, index){
+            objProp[key] = array[index];
+        });
+        // console.log(objProp);
+
+        users.splice([i], 1, objProp);
+        console.log(users);
+
+        saveDataToLocalStorage();
+        updateMainTable();
+    });
+
+    userInfoTable.append(userInfoSaveButton);
+}
+
+function showAddNewUserCard(i) {
+        infoWindow.innerHTML = '';
+
+        let userInfoTable = document.createElement('table');
+        userInfoTable.classList.add('table');
+        userInfoTable.classList.add('table-newUser');
+        userInfoTable.innerHTML = 'ADD NEW USER';
+        infoWindow.append(userInfoTable);
+
+        let userInfoCell;
+        let userInfoRow;
+        let userEditInput;
+        for (let key in users[i]) {
+            userInfoRow = document.createElement('tr');
+            userInfoRow.classList.add('table__row');
+            userInfoTable.append(userInfoRow);
+        
+            userInfoCell = document.createElement('td'); 
+            userInfoCell.classList.add('table__cell');
+            userInfoCell.innerHTML = `${key}:<br>`;
+            userInfoRow.append(userInfoCell);
+    
+            userEditInput = document.createElement('input');
+            userEditInput.setAttribute('value', '');
+
+            userEditInput.classList.add('edit__inputs');
+            userInfoTable.append(userEditInput);
+        }
+
+        let userInfoCloseButton = document.createElement('input');
+        userInfoCloseButton.setAttribute('type', 'button');
+        userInfoCloseButton.setAttribute('value', 'Close');
+        userInfoCloseButton.classList.add('table__button');
+        userInfoCloseButton.addEventListener('click', () => closeWindow(userInfoTable));
+        userInfoTable.append(userInfoCloseButton);
+
+        let userInfoSaveButton = document.createElement('input');
+        userInfoSaveButton.setAttribute('type', 'button');
+        userInfoSaveButton.setAttribute('value', 'Save');
+        userInfoSaveButton.classList.add('table__button');
+        userInfoSaveButton.addEventListener('click', function() {
+            let userInputs = document.querySelectorAll('.edit__inputs');
+            let array       = Object.values(users[i]);
+    
+            for (let i = 0; i < userInputs.length; i++) {
+                if (array[i] !== userInputs[i].value) {
+                    array[i] = userInputs[i].value;
+                } 
+            }
+ 
+            const objProp = {id: null, name: null, age: null, job: null};
+            Object.keys(objProp).forEach(function(key, index){
+                objProp[key] = array[index];
+            });
+
+            users.push(objProp);
+            console.log(users);
+    
+            saveDataToLocalStorage();
+            updateMainTable();
+        });
+        userInfoTable.append(userInfoSaveButton);
+}
+
+function closeWindow(itemToRemove) {
+    itemToRemove.remove();
+}
+
+function saveDataToLocalStorage() {
+    let stringifiedArray = JSON.stringify(users);
+    localStorage.setItem('Users', stringifiedArray);
+}
+
+function getDataFromLocalStorage() {
+    const getDataFromLocalStorage = document.getElementById('get-data');
+    getDataFromLocalStorage.addEventListener('click', function () {
+        localStorage.getItem('Users');
+        let parsedArray = JSON.parse('Users');
+        users = parsedArray;
+        saveDataToLocalStorage();
+        updateMainTable();
+        console.log(users);
+    });
+}
+
+function updateMainTable() {
+    table.innerHTML = '';
+
+    
+    let headingRow = document.createElement('tr');
+    headingRow.classList.add('table__row');
+    table.append(headingRow);
+    
+    let headingCellId = document.createElement('th');
+    headingCellId.classList.add('table__heading-cell');
+    headingCellId.innerHTML = 'ID';
+    headingRow.append(headingCellId);
+
+    let headingCellName = document.createElement('th');
+    headingCellName.classList.add('table__heading-cell');
+    headingCellName.innerHTML = 'NAME';
+    headingRow.append(headingCellName);
+
+    let headingCellActinos = document.createElement('th');
+    headingCellActinos.classList.add('table__heading-cell');
+    headingCellActinos.innerHTML = 'ACTIONS';
+    headingRow.append(headingCellActinos);
+
+    for (let i = 0; i < users.length; i++) {
+        let row = document.createElement('tr');
+        row.classList.add('table__row');
+        table.append(row);
+
+        let cellId = document.createElement('td');
+        cellId.classList.add('table__cell');
+        row.append(cellId);
+
+        cellId.innerText = users[i].id;
+
+        let cellName = document.createElement('td');
+        cellName.classList.add('table__cell');
+        row.append(cellName);
+
+        cellName.innerText = users[i].name;
+
+        let cellActions = document.createElement('td');
+        cellActions.classList.add('table__cell');
+        row.append(cellActions);
+
+        let tableButtonView   = createRowActionButton('View');
+        tableButtonView.addEventListener('click', () => showUserInfoCard(i));
+
+        let tableButtonEdit   = createRowActionButton('Edit');
+        tableButtonEdit.addEventListener('click', () => showUserEditCard(i));
+
+        let tableButtonDelete = createRowActionButton('Delete');
+
+        tableButtonAdd.addEventListener('click', () => showAddNewUserCard(i));
+
+        cellActions.append(tableButtonView);
+        cellActions.append(tableButtonEdit);
+        cellActions.append(tableButtonDelete);
+    }
+}
+
+function removeUserObject(i) {
+    users.splice(users[i], 1);
+    saveDataToLocalStorage();
+    updateMainTable();
+}
+
+// function getDataFromLocalStorage() {
+//     if (!localStorage.getItem('Users')) {
+//         return defaultUsers;
+//     }
+    
+//     let parsedUsers = JSON.parse(localStorage.getItem('Users'));
+//     if (!parsedUsers) {
+//         return defaultUsers;
+//     }
+//     return parsedUsers;
+// }
+
--- js/variables.js
+++ js/variables.js
@@ -1,16 +1,12 @@
 'use strict';
 
 const users = [
-    { id: '1', name: 'Jonh', age: 30, job: 'developer' },
-    { id: '2', name: 'Bill', age: 28, job: 'singer'    },
-    { id: '3', name: 'Kate', age: 23, job: 'racer'     },
+    { id: '1', name: 'Jonh', age: '30', job: 'developer' },
+    { id: '2', name: 'Bill', age: '28', job: 'singer'    },
+    { id: '3', name: 'Kate', age: '23', job: 'racer'     },
 ];
 
+const tableWrap      = document.getElementById('table-wrap');
+const table          = document.getElementById('table');
+const infoWindow     = document.getElementById('info-window');
 const tableButtonAdd = document.getElementById('table__button-add');
-
-const table = document.getElementById('table');
-
-// const tableButtonView = createRowActionButton('View');
-// const tableButtonEdit = createRowActionButton('Edit');
-// const tableButtonDelete = createRowActionButton('Delete');
-
